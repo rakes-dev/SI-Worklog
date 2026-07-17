3f84a6d973d11560c7087eb1b8757068519261c0
Rakesh <rakesh374@outlook.com>
2026-07-17 11:46:10 +0530

    Update

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\rakesh-dev\Desktop\SI-Worklog\src\app\page.tsx
+// File: C:\Users\Rakesh\Desktop\SI Worklog\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
